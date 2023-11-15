c0c9cf8f88cf1c6a9fa3946fd5b183d0504cb419
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-15 19:15:18 +0100

    wip

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Welcome");
+
+const ROCK = 0;
+const PAPER = 1;
+const SCISSOR = 2;
+const OPTIONS = ["rock", "paper", "scissor"];
+
+function rockClicked() {
+    checkResult(ROCK);
+}
+
+function paperClicked() {
+    checkResult(PAPER);
+}
+
+function scissorClicked() {
+    checkResult(SCISSOR);
+}
+
+function getComputerChoice() {
+    return randomIntFromInterval(0, OPTIONS.length - 1);
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function checkResult(player_choice) {
+    const computer_choice = getComputerChoice();
+
+    console.log("Your choice: ", OPTIONS[player_choice]);
+    console.log("Computer choice: ", OPTIONS[computer_choice]);
+
+    if (player_choice === computer_choice) {
+        console.log("tie");
+    }
+    else if (player_choice + 1 === computer_choice) {
+        console.log("you win");
+    }
+    else {
+        console.log("you lose");
+    }
+}
